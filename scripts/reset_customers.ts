40d126489d1efff496b5809a3b4603d117d0c62a
SmartFlow Dev <dev@smartflow.africa>
2026-02-01 15:39:48 +0000

    Fix: Switch reset script to CommonJS require syntax to avoid ts-node ESM errors

--- scripts/reset_customers.ts
+++ scripts/reset_customers.ts
@@ -1,7 +1,8 @@
 
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
+
 
 async function clearCustomers() {
     try {
